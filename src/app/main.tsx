126778f5af1a94d984a5105a521fefef7d15e683
Roman Sushevskij <roma.sushevskij@yandex.ru>
2025-04-23 13:59:12 +0300

    Добавление confirm dialog для удаления пользователя

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from "@toolpad/core/AppProvider";
 
 import App from "./App";
 import "./styles/index.css";
+import { createTheme } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppProvider theme={createTheme({ colorSchemes: { light: true } })}>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </StrictMode>,
 );
--- src/features/users/delete/ui/delete-user-btn.tsx
+++ src/features/users/delete/ui/delete-user-btn.tsx
@@ -1,13 +1,26 @@
 import Button from "@mui/material/Button";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { useDialogs } from "@toolpad/core/useDialogs";
 
 import { TUser, useUsersStore } from "@/entities/users";
 
 export const DeleteUserBtn = ({ userId }: { userId: TUser["id"] }) => {
+  const dialogs = useDialogs();
   const { deleteUser } = useUsersStore();
 
-  const handleDeleteUser = () => {
-    deleteUser(userId);
+  const handleDeleteUser = async () => {
+    const isDeleteConfirmed = await dialogs.confirm(
+      "Вы уверены, что хотите удалить пользователя?",
+      {
+        okText: "Удалить",
+        cancelText: "Отмена",
+        title: "",
+      },
+    );
+
+    if (isDeleteConfirmed) {
+      deleteUser(userId);
+    }
   };
 
   return (
